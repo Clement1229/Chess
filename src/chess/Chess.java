8fb986147356dd6ee05236b790a511f62d6315a0
Clement Lin <x4383lin@gmail.com>
2017-05-02 20:08:41 -0500

    package player && all kinds of chess

--- src/chess/Chess.java
+++ src/chess/Chess.java
@@ -1,5 +1,7 @@
 package chess;
 
 public class Chess {
+	
+	
 
 }
--- src/chessBoard/ChessBoard.java
+++ src/chessBoard/ChessBoard.java
@@ -2,4 +2,12 @@ package chessBoard;
 
 public class ChessBoard {
 
+	
+	public ChessBoard(){
+		initialize();
+	}
+	
+	public void initialize(){
+		
+	}
 }
